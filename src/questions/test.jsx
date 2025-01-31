d32bcbcc3423d8c5018947e1f9e4196f44e7fbaf
marksikaundi <emmanuelmark117@gmail.com>
2025-01-31 11:21:46 +0000

    ✅ Add unit tests for questions component

---
+++ src/questions/test.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+
+function TestSample() {
+
+    //Init function
+   // eslint-disable-next-line no-unused-vars
+   const [state, setState] = useState('')
+
+    //UseEffect function
+
+
+    //Render function
+
+    //Return
+
+
+
+  return (
+    <div>
+        <button >
+            Click me
+        </button>
+      
+    </div>
+  )
+}
+
+export default TestSample
